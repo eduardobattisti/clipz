884c327c8c4e41af1431ce03fedacafb106eeb2a
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-21 11:30:16 -0300

    refactor: update app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clips'`, () => {
+  it(`should have as title 'clipz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('clips');
+    expect(app.title).toEqual('clipz');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('clips app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('clipz app is running!');
   });
 });
