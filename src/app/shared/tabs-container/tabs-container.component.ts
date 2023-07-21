0f75baeac62b7dfc27a2cd246db22d23132f023c
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-21 16:33:13 -0300

    feat: implements selection tab logic

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, AfterContentInit, QueryList, Query } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.scss']
 })
-export class TabsContainerComponent {
+export class TabsContainerComponent implements AfterContentInit {
 
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>;
+
+  constructor() {}
+
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(tab => tab.active);
+
+    if (!activeTabs?.length) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  public selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false;
+    })
+
+    tab.active = true;
+
+    return false;
+  }
+
+  public getTabClasses(tab: TabComponent) {
+    return {
+      'hover:text-indigo-400': !tab.active,
+      'hover:text-white text-white bg-indigo-400': tab.active
+    }
+  }
 }
