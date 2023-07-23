5fb20f3f228d7efb3ca9eace18fcb92c2b0b82d1
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-23 20:24:04 -0300

    feat(WIP): create environment according ng structure

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyChHGeCTt9ogU67beIpee2G7jhhG2914Jg",
+    authDomain: "clipz-17be0.firebaseapp.com",
+    projectId: "clipz-17be0",
+    storageBucket: "clipz-17be0.appspot.com",
+    appId: "1:837380616413:web:ebc4513991c05b5556ab34"
+  }
+};
