7db282c922adf5dbf32f418f24626f08970d7f11
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-23 20:24:29 -0300

    feat(WIP): implements firebase account creation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  inSubmission = false;
+
   name = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -45,9 +48,28 @@ export class RegisterComponent {
     phone_number: this.phone_number,
   });
 
-  register() {
+  constructor(private auth: AngularFireAuth) {}
+
+  public async register() {
     this.showAlert = true;
     this.alertMsg = 'Please wait! Your account is being created';
     this.alertColor = 'blue';
+
+    const { email, password } =  this.registerForm.value;
+
+    try {
+      const userCredentials = await this.auth.createUserWithEmailAndPassword(
+        email as string, password as string
+      );
+      console.log(userCredentials);
+      this.alertMsg = 'Success! Your account has been created.';
+      this.alertColor = 'green';
+    } catch(error) {
+      this.showAlert = true;
+      this.alertMsg = 'An unexpected error occurred. Please try again later.';
+      this.alertColor = 'red';
+    } finally {
+      this.inSubmission = false;
+    }
   }
 }
