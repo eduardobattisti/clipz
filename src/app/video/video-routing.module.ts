7287cc937b597793ab0690325b0119e27592ae13
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-24 18:13:53 -0300

    feat: add route guards on protected routes

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -2,21 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
+
+const authorizedRoutesStructure = {
+  data: {
+    authOnly: true,
+    authGuardPipe: redirectUnauthorizedToHome
+  },
+  canActivate: [AngularFireAuthGuard]
+}
 
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
-    data: {
-      authOnly: true
-    }
+    ...authorizedRoutesStructure
   },
   {
     path: 'upload',
     component: UploadComponent,
-    data: {
-      authOnly: true
-    }
+    ...authorizedRoutesStructure
   }
 ];
 
