09c2d26e7b5974ddfb39daacc1f34c888670c88c
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-24 00:08:52 -0300

    feat: create attribute to handle collection

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private usersCollection: AngularFirestoreCollection<IUser>;
 
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
-  ) {}
+  ) {
+    this.usersCollection = this.db.collection<IUser>('users');
+  }
 
   public async createUser(userData: IUser) {
     const { name, email, password, age, phoneNumber: phoneNumber } = userData;
 
+    if (!password) {
+      throw new Error('Password not provided!');
+    }
+
     await this.auth.createUserWithEmailAndPassword(email, password);
 
-    await this.db.collection<IUser>('users').add({
+    await this.usersCollection.add({
       name,
       email,
       age,
