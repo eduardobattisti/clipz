098d252ef82c883f290d1ed34dbcff14240f06ea
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-25 15:41:42 -0300

    feat: add screenshot selection

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -29,6 +29,7 @@ export class UploadComponent implements OnDestroy {
   user: firebase.User | null =  null;
   task?: AngularFireUploadTask;
   screenshots: string[] = [];
+  selectedScreenshot = '';
   title = new FormControl('', {
     validators: [
       Validators.required,
@@ -58,6 +59,10 @@ export class UploadComponent implements OnDestroy {
   }
 
   async storeFile($event: Event) {
+    if(this.ffmpegService.isRunning) {
+      return;
+    }
+
     this.isDragover = false;
 
     this.file = ($event as DragEvent).dataTransfer
@@ -69,6 +74,8 @@ export class UploadComponent implements OnDestroy {
     }
 
     this.screenshots = await this.ffmpegService.getScreenshots(this.file);
+
+    this.selectedScreenshot = this.screenshots[0];
 
     this.uploadForm.get('title')?.setValue(
       this.file.name.replace(/\.[^/.]+$/, '')
