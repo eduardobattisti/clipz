c781ec0233663acd8e2a8b716ea73f28afa91b82
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-25 00:08:51 -0300

    feat: implement sort logic

--- src/app/services/clip.service.ts
+++ src/app/services/clip.service.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { switchMap, of, map } from 'rxjs';
+import { switchMap, of, map, BehaviorSubject, combineLatest } from 'rxjs';
 
 import type IClip from '../models/clips.model';
 
@@ -29,15 +29,23 @@ export class ClipService {
     return this.clipsCollection.add(data);
   }
 
-  getUserClips() {
-    return this.auth.user.pipe(
-      switchMap(user => {
-        if (!user) {
-          return of([])
+  getUserClips(sort$: BehaviorSubject<string>) {
+    return combineLatest([
+      this.auth.user,
+      sort$
+    ]).pipe(
+      switchMap(values => {
+        const [user, sort] = values;
+
+        if(!user) {
+          return of([]);
         }
 
         const query = this.clipsCollection.ref.where(
           'uid', '==', user.uid
+        ).orderBy(
+          'timestamp',
+          sort === '1' ? 'desc' : 'asc'
         );
 
         return query.get();
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ClipService } from 'src/app/services/clip.service';
 import { ModalService } from 'src/app/services/modal.service';
+import { BehaviorSubject } from 'rxjs';
 
 import type IClip from 'src/app/models/clips.model';
 
@@ -14,20 +15,24 @@ export class ManageComponent {
   videoOrder = '1';
   clips: IClip[] = [];
   activeClip: IClip | null = null;
+  sort$: BehaviorSubject<string>;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private clipService: ClipService,
     private modalService: ModalService
-  ) {}
+  ) {
+    this.sort$ = new BehaviorSubject(this.videoOrder);
+  }
 
   ngOnInit(): void {
     this.route.queryParamMap.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : '1';
+      this.sort$.next(this.videoOrder);
     });
 
-    this.clipService.getUserClips().subscribe(docs => {
+    this.clipService.getUserClips(this.sort$).subscribe(docs => {
       this.clips = [];
 
       docs.forEach(doc => {
@@ -36,7 +41,7 @@ export class ManageComponent {
           ...doc.data()
         })
       })
-    })
+    });
   }
 
   sort(event: Event) {
