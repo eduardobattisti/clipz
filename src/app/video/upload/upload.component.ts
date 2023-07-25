691457f7d182e6fa12276817f0fa26b13265a3b2
eduardobattisti <eduardobattisti10@gmail.com>
2023-07-24 22:04:58 -0300

    feat(WIP): add subscriber to handle file meta data

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 import { v4 as uuid } from 'uuid';
-import { last } from 'rxjs';
+import { last, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-upload',
@@ -21,6 +23,7 @@ export class UploadComponent {
   allowedFileMimeType = 'video/mp4';
   percentage = 0;
   showPercentage = false;
+  user: firebase.User | null =  null;
   title = new FormControl('', {
     validators: [
       Validators.required,
@@ -34,7 +37,12 @@ export class UploadComponent {
     title: this.title
   })
 
-  constructor(private storage: AngularFireStorage) { }
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth
+  ) {
+    auth.user.subscribe(user => this.user = user);
+  }
 
   storeFile($event: Event) {
     this.isDragover = false;
@@ -63,15 +71,27 @@ export class UploadComponent {
     const clipPath = `clips/${clipFileName}.mp4`;
 
     const task = this.storage.upload(clipPath, this.file);
+    const clipReference = this.storage.ref(clipPath);
 
     task.percentageChanges().subscribe((progress) => {
       this.percentage = progress as number / 100;
     });
 
     task.snapshotChanges().pipe(
-      last()
+      last(),
+      switchMap(() => clipReference.getDownloadURL())
     ).subscribe({
-      next: (snapshot) => {
+      next: (url) => {
+        const clip = {
+          uid: this.user?.uid,
+          displayName: this.user?.displayName,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url
+        }
+
+        console.log(clip)
+
         this.alertColor = 'green';
         this.alertMsg = 'Success! Your clip is now ready to share with the world.';
         this.showPercentage = false;
@@ -83,6 +103,6 @@ export class UploadComponent {
         this.showPercentage = false;
         console.error(error);
       }
-    })
+    });
   }
 }
